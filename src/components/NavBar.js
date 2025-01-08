dca211f915f7cd46c72f50fbd194d2b19c815bd1
Aria Shi <ariashi15@gmail.com>
2025-01-08 00:11:33 -0600

    changed navbar to custom sizes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,18 +10,18 @@ export default function NavBar() {
     }, [selectedNavItem]);
   
     return (
-      <div id="nav-bar" className="flex items-center pr-12 font-title relative">
-        <div id="nav-text" className="flex flex-col gap-20 mr-14">
+      <div id="nav-bar" className="flex items-center mr-[60px] font-title relative">
+        <div id="nav-text" className="flex flex-col gap-[80px] mr-[56px]">
           {navItems.map((item, index) => (
-            <div key={item} className="flex items-center justify-end gap-10">
+            <div key={item} className="flex items-center justify-end gap-[40px]">
               <div 
-                className={`w-full text-right transform transition-all duration-300 origin-right ${selectedNavItem === item ? "text-3xl text-dark-purple" : "text-2xl text-dark-blue"} hover:text-3xl hover:text-dark-purple`}
+                className={`w-full text-right transform transition-all duration-300 origin-right ${selectedNavItem === item ? "text-[30px] text-dark-purple" : "text-[24px] text-dark-blue"} hover:text-[30px] hover:text-dark-purple`}
                 onClick={() => setSelectedNavItem(item)}
                 >{item}</div>
             </div>
           ))}
         </div>
-        <div id="nav-line" className="relative flex items-center justify-center w-0.5 h-screen">
+        <div id="nav-line" className="relative flex items-center justify-center w-[2px] h-screen">
           <Dots />
           <div id="vertical-line" className="absolute w-full h-full bg-dark-blue"></div>
         </div>
@@ -34,14 +34,14 @@ export default function NavBar() {
 
     for (let i = 0; i < 5; i++) {
       dots.push(
-      <div className="flex h-8 items-center justify-center">
-        <div className="absolute w-2 h-2 rounded-full bg-dark-blue"></div>
+      <div className="flex h-[32px] items-center justify-center">
+        <div className="absolute w-[8px] h-[8px] rounded-full bg-dark-blue"></div>
       </div>
       );
     }
 
     return (
-      <div className="absolute w-full flex flex-col items-center gap-20">
+      <div className="absolute w-full flex flex-col items-center gap-[85px]">
         {dots}
       </div>
     );
